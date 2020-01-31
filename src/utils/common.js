d8e8771ec298b5ab56d06d2cbb910544c0231449
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-01-31 11:38:30 +0200

    adds debounce

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,6 +1,21 @@
 const MINUTES_IN_HOUR = 60;
+const DEBOUNCE_TIMEOUT = 500;
 
 export default class Common {
+  static debounce(callback) {
+    let lastTimeout = null;
+
+    return (...parameters) => {
+
+      if (lastTimeout) {
+        clearTimeout(lastTimeout);
+      }
+
+      lastTimeout = setTimeout(() => {
+        callback(...parameters);
+      }, DEBOUNCE_TIMEOUT);
+    };
+  }
   static calcUserRank(amountWatched, minValRank, middleJrValRank, middleValRank, maxValRank) {
     let rank = ``;
 
@@ -17,12 +32,12 @@ export default class Common {
 
   static getTimeInHoursAndMinutes(timeInMin) {
     const runtime = {
-      hours: ``,
-      minutes: ``,
+      hours: `0`,
+      minutes: `0`,
     };
 
     if (timeInMin < MINUTES_IN_HOUR) {
-      runtime.minute = (timeInMin < 10) ? `0${timeInMin}` : timeInMin;
+      runtime.minutes = (timeInMin < 10) ? `0${timeInMin}` : timeInMin;
       return runtime;
     }
 
